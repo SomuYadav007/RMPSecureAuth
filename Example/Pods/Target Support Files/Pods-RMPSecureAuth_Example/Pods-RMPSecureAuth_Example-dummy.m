8670f381e502b06dc3011b1cb7c6e792072d5c9a
somu yadav <somu.yadav@innoeye.com>
2020-07-08 15:11:32 +0530

    Header added

---
+++ Example/Pods/Target Support Files/Pods-RMPSecureAuth_Example/Pods-RMPSecureAuth_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RMPSecureAuth_Example : NSObject
+@end
+@implementation PodsDummy_Pods_RMPSecureAuth_Example
+@end
---
+++ Example/Pods/Target Support Files/RMPSecureAuth/RMPSecureAuth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_RMPSecureAuth : NSObject
+@end
+@implementation PodsDummy_RMPSecureAuth
+@end
